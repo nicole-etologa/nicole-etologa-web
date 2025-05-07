14b7f588a89f0adc2c7fd0fe48d594900b1e6354
Michel Adasme <mic.adasme@gmail.com>
2025-05-07 19:12:23 -0400

    validations PriceDisplay, overwrite taller

--- src/data/workshops.ts
+++ src/data/workshops.ts
@@ -16,7 +16,7 @@ export interface Workshop {
   export const workshops: Workshop[] = [
     {
       id: "taller-caos-calma-felina",
-      title: "Taller grupal en vivo - Del caos a la calma felina",
+      title: "Taller Grabado - Del caos a la calma felina",
       description: "🐾 Un taller para sanar la relación entre tus gatos 🐾",
       fullDescription: "¿Tus gatos pelean, se evitan o viven en tensión constante? 😿💢 <br> Este taller es para ti si sueñas con una convivencia armoniosa, sin conflictos ni estrés entre tus michis. 🧘‍♀️🐱💕",
       included: [
@@ -27,17 +27,14 @@ export interface Workshop {
         "Cómo fomentar una convivencia más pacífica y equilibrada.",
       ],
       bonuses: [
-        "💬 Sección de preguntas y respuestas en vivo para resolver tus dudas.",
-        "🎉 Sorteo de una asesoría personalizada de 2 horas (vía Zoom) entre los asistentes.",
         "🎥 Grabación del taller con acceso de por vida.",
-        "🎊 Regalos sorpresa.",
-        "📘 Guía descargable del protocolo de reintroducción o reparación del vínculo felino."  
+        "📘 Guía descargable del protocolo de reintroducción o reparación del vínculo felino." 
       ],
-      dateStart:"Lunes 5 de mayo del 2025 - 20:00 Hora de Chile.",
-      duration: "2,5 horas aproximadamente.",
-      mode:"Taller grupal en vivo vía Zoom.",
-      originalPrice: "19",
-      price: "17",
-      link: "https://wa.me/56947023420?text=Hola Nicole, me interesa inscribirme para el Taller del caos a la calma felina."
+      dateStart:"",
+      duration: "",
+      mode:"",
+      originalPrice: "",
+      price: "22",
+      link: "https://wa.me/56947023420?text=Hola Nicole, me interesa el Taller grabado - Del caos a la calma felina."
     }
   ];
